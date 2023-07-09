f538d223b9b68f413ea785d9d73ebb6249d56090
jacksonpicpay <jackson.neves@picpay.com>
2023-07-09 12:27:35 -0300

    added custom deploy action (S3)

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,22 @@
+// `npm init -y`
+// `npm install @actions/core @actions/github @actions/exec`
+
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // 1) get some input values.
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2) upload files.
+  const s3Uri = `s3://${bucket}`;
+
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+  // exec.exec('aws s3 sync <local-folder> <s3-bucket>');
+
   core.notice('Hello from my custom JavaScript Action!');
 }
 
